7db41f19c0dd990a858a6ab85e2da26594ffe0c8
ahiru <nmaeda-nit24@yb4.so-net.ne.jp>
2014-11-25 14:09:11 +0900

    Update ahiru_yakuna.rb

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -5,8 +5,8 @@ Plugin.create(:ahiru_yakuna) do
   on_appear do |ms|
     ms.each do |m|
       if m.message.to_s =~ /あひる焼き/
-        replySentence = replyArray[rand(ary.length)] 
-        Service.primary.post(:message => "#{"@" + m.user.idname} ", :replyto => m, replySentence)
+        replySentence = replyArray.sample
+        Service.primary.post(:message => "#{"@" + m.user.idname} ", :replyto => m replySentence)
         m.message.favorite(true)
       end
     end
