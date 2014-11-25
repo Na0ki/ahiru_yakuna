9bd8e4147c1bc0ecf17b43fadbc44a3c0c1d8c26
FirstSpring <firstspring1845@gmail.com>
2014-11-25 22:22:11 +0900

    RESTで拾った古いツイに反応しないように変更

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 Plugin.create(:ahiru_yakuna) do
+
+  DEFINED_TIME = Time.new.freeze
+
   replyArray = ["焼くな", "おう、火力足んねぇぞ！", "延焼してどうぞ", "焼いちゃらめえええええええ", "焼いちゃうのか？！本当に焼いちゃうのか？！", "貴様はすでに延焼している"]
   on_appear do |ms|
     ms.each do |m|
-      if m.message.to_s =~ /あひる焼き/
+      if m.message.to_s =~ /あひる焼き/ and m[:created] > DEFINED_TIME
         replySentence = replyArray.sample
         Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + replySentence}", :replyto => m)
         m.message.favorite(true)
