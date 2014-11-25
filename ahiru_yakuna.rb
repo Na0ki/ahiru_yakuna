081b2f41723a6460f4e7cd6410e1323c93fcc519
ahiru <nmaeda-nit24@yb4.so-net.ne.jp>
2014-11-25 14:01:12 +0900

    Update ahiru_yakuna.rb

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 Plugin.create(:ahiru_yakuna) do
+  replyArray = ["焼くな", "おう、火力足んねぇぞ！", "延焼してどうぞ", "焼いちゃらめえええええええ", "焼いちゃうのか？！本当に焼いちゃうのか？！", "貴様はすでに延焼している"]
   on_appear do |ms|
     ms.each do |m|
       if m.message.to_s =~ /あひる焼き/
-        Service.primary.post(:message => "#{"@" + m.user.idname} 焼くな", :replyto => m)
+        replySentence = replyArray[rand(ary.length)] 
+        Service.primary.post(:message => "#{"@" + m.user.idname} ", :replyto => m, replySentence)
         m.message.favorite(true)
       end
     end
