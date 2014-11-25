7294fa42b7ed79e785814bacca92e22a49a48987
ahiru <nmaeda-nit24@yb4.so-net.ne.jp>
2014-11-25 20:14:15 +0900

    Update ahiru_yakuna.rb

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -5,12 +5,17 @@ Plugin.create(:ahiru_yakuna) do
   on_appear do |ms|
     ms.each do |m|
       if m.message.to_s =~ /あひる焼き/
-        unless m.user.idname == m.me then
-          return
-        else
+        if !m.retweet? && !m.user.is_me?
           replySentence = replyArray.sample
           Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + replySentence}", :replyto => m)
           m.message.favorite(true)
+          if m.retweet?
+          if m.retweet_source.user.is_me? == false
+            m.favorite(true)
+          end
+          elsif m.user.is_me? == false
+            m.favorite(true)
+          end
         end
       end
     end
