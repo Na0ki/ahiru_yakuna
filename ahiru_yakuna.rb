8d8a70f7a07bdd875780adbcf2f6303090739845
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-11-30 02:59:54 +0900

    use config.yml instead of too long one line array

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -4,7 +4,12 @@ Plugin.create(:ahiru_yakuna) do
 
   DEFINED_TIME = Time.new.freeze
 
-  replyArray = ["あ、ミディアムレアでよろしく","我が輩を焼くなど897年早いわ！！！","不完全燃焼","Wer meines Speeres Spitze furchtet, durchschreite das feuer nie!","unlimited blaze works","「――――I am the bone of my roaring flames. 体は　　炎で　出来ている」","小便は済ませたか？神様にお祈りは？部屋の隅でガタガタ震えて燃焼する心の準備はOK？","邪王炎殺黒龍波","闇の炎に抱かれて燃えろ","あひる焼くな細胞は存在します","誰が誰にあひる焼きジデモ゛オンナジヤ、オンナジヤ思っでえ！ ウーハッフッハーン！！ ッウーン！","明日は我が身","バーニング・ダーク・フレイム・オブ・ディッセンバー","らこらこらこ～w","ｙａｋａｒｅｓｕｇｉ（ｋａｋｕｊｉｔｕｎｉ）","そんなにっ！焼いたらっ！ｱﾝｯ!ｱﾝｯ!ｱﾝｯ!ｱﾝｯ!","あ、これ焼かれてるんじゃないんで。ちょっとトランザムしてるだけなんで。","天照","焼きすぎ注意報","溶鉱炉に突っ込むね☆","あなたのプロジェクト、炎上しますよ？","焼かないで焼かないで震えといて","あふん","焼くでない", "おう、火力足んねぇぞ！", "延焼してどうぞ", "焼いちゃらめえええええええ", "焼いちゃうのか？！本当に焼いちゃうのか？！", "貴様はすでに延焼している"]
+  begin
+    replyArray = YAML.load_file(File.join(File.dirname(__FILE__),"config.yml"))
+  rescue LoadError
+    notice "\"config.yml\" not found."
+  end
+
   on_appear do |ms|
     ms.each do |m|
       if m.message.to_s =~ /あひる焼き/ and m[:created] > DEFINED_TIME and !m.retweet?
