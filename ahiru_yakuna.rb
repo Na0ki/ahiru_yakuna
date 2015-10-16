1c7ef4ec2e9b50d2a6d7e2398a4370a9462918e4
ahiru <nmaeda-nit24@yb4.so-net.ne.jp>
2015-10-17 00:00:09 +0900

    Update ahiru_yakuna.rb

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -3,26 +3,26 @@
 Plugin.create(:ahiru_yakuna) do
  
   DEFINED_TIME = Time.new.freeze
-  now = Time.now
-  
-  begin
- 	 replyArray = YAML.load_file(File.join(File.dirname(__FILE__),"config.yml"))
- 	 meshiteroArray = YAML.load_file(File.join(File.dirname(__FILE__),"meshitero.yml"))
-  rescue LoadError
-   notice "\"config.yml\" not found."
-  end
  
-  on_appear do |ms|
-   ms.each do |m|
-   if m.message.to_s =~ /あひる焼き|ahiruyaki/ and m[:created] > DEFINED_TIME and !m.retweet?
-    if (now.hour < 19 && now.hour > 17) || (now.hour > 0 && now.hour < 3) then
-     replySentence = meshiteroArray.sample
-    else
-    　replySentence = replyArray.sample
-    end
-    Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + replySentence}", :replyto => m)
-    m.message.favorite(true)
-   end
+  begin
+	replyArray = YAML.load_file(File.join(File.dirname(__FILE__),"config.yml"))
+	meshiteroArray = YAML.load_file(File.join(File.dirname(__FILE__),"meshitero.yml"))
+  rescue LoadError
+   	notice "\"config.yml\" not found."
   end
- end
+
+  on_appear do |ms|
+    ms.each do |m|
+      if m.message.to_s =~ /あひる焼き|ahiruyaki|扒家鸭/ and m[:created] > DEFINED_TIME and !m.retweet?
+      	now = Time.now.hour
+      	if (now > 17 && now < 19) || (now > 0 && now < 3) then
+        	replySentence = meshiteroArray.sample
+        else
+        	replySentence = replyArray.sample
+        end
+        Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + replySentence}", :replyto => m)
+        m.message.favorite(true)
+      end
+    end
+  end
 end
