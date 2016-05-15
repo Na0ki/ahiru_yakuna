3b03dc3d35cdf73709ab24c089e955860ee209d2
Na0ki <wellingtonnmaeda0524@gmail.com>
2016-05-15 16:17:13 +0900

    update

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -7,6 +7,8 @@ Plugin.create(:ahiru_yakuna) do
   begin
 	replyArray = YAML.load_file(File.join(File.dirname(__FILE__),"config.yml"))
 	meshiteroArray = YAML.load_file(File.join(File.dirname(__FILE__),"meshitero.yml"))
+    steinArray = YAML.load_file(File.join(File.dirname(__FILE__),"stein.yml"))
+    chineseArray = YAML.load_file(File.join(File.dirname(__FILE__),"chinese.yml"))
   rescue LoadError
    	notice "\"config.yml\" not found."
   end
@@ -15,13 +17,28 @@ Plugin.create(:ahiru_yakuna) do
     ms.each do |m|
       if m.message.to_s =~ /あひる焼き|Ahiruyaki|扒家鸭/ and m[:created] > DEFINED_TIME and !m.retweet?
       	now = Time.now.hour
-      	if (now > 17 && now < 19) || (now > 0 && now < 3) then
+      	if (now >= 17 && now <= 19) || (now >= 0 && now <= 3) then
         	replySentence = meshiteroArray.sample
         else
+          if m.message.to_s =~ /扒家鸭/
+            replySentence = chineseArray.sample
+          else
         	replySentence = replyArray.sample
+          end
         end
-        Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + replySentence}", :replyto => m)
-        m.message.favorite(true)
+        # if m.user.idname == "naota344"
+        #   Service.primary.send_direct_message(text: replySentence,
+                                            #   user: m.user.idname)
+        # else
+          Service.primary.post(:message => "#{"@" + m.user.idname + ' ' + replySentence}", :replyto => m)
+          m.message.favorite(true)
+        # end
+      end
+      if m.message.to_s =~ /ｳﾞｪﾝﾃﾞﾙｼｭﾀｲﾝ焼き/ and m[:created] > DEFINED_TIME and !m.retweet?
+        replySentence = steinArray.sample
+        if m.user.idname == "ahiru3net" || m.user.idname == "wendelstein__"
+          Service.primary.post(:message => "#{"@" + "wendelstein__" + ' ' + replySentence}", :replyto => m)
+        end
       end
     end
   end
