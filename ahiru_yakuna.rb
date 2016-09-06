7691c817c797a4d0dfec79740b3c1d2e120abe4a
ahiru <wellingtonnmaeda0524@gmail.com>
2016-09-06 14:58:59 +0900

    英語追加 言語の増加により、返信を選択するメソッドを追加

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -8,34 +8,43 @@ Plugin.create(:ahiru_yakuna) do
 
   # load reply dictionaries
   begin
-    default = YAML.load_file(File.join(__dir__, 'replydic', 'default.yml'))
-    meshitero = YAML.load_file(File.join(__dir__, 'replydic', 'meshitero.yml'))
-    chinese = YAML.load_file(File.join(__dir__, 'replydic', 'chinese.yml'))
+    JAPANESE = YAML.load_file(File.join(__dir__, 'replydic', 'default.yml'))
+    MESHITERO = YAML.load_file(File.join(__dir__, 'replydic', 'meshitero.yml'))
+    ENGLISH = YAML.load_file(File.join(__dir__, 'replydic', 'english.yml'))
+    CHINESE = YAML.load_file(File.join(__dir__, 'replydic', 'chinese.yml'))
   rescue LoadError
     notice 'Could not load yml file'
   end
 
   filter_filter_stream_track do |watching|
-    [(watching.split(','.freeze) + あひる焼き).join(",")]
+    [(watching.split(','.freeze) + あひる焼き).join(',')]
   end
+
+
+  def select_reply(msg, time)
+    # 飯テロモード
+    if (time >= 17 && time <= 19) || (time >= 0 && time <= 3)
+      return MESHITERO.sample
+    end
+
+    # 言語ごとに使用辞書を変える
+    if msg =~ /Ahiruyaki/
+      return ENGLISH.sample
+    elsif msg =~ /扒家鸭/
+      return CHINESE.sample
+    else
+      return JAPANESE.sample
+    end
+  end
+
 
   on_appear do |ms|
     ms.each do |m|
       if m.to_s =~ Regexp.union(あひる焼き) and m[:created] > DEFINED_TIME and !m.retweet? and !criminals.include?(m.id)
         criminals << m.id
         now = Time.now.hour
-        # select reply dic & get sample reply
-        if (now >= 17 && now <= 19) || (now >= 0 && now <= 3)
-          reply = meshitero.sample
-        else
-          if m.to_s =~ /扒家鸭/
-            reply = chinese.sample
-          else
-            reply = default.sample
-          end
-        end
-
-        # send reply & fav
+        # select reply dic & send reply & fav
+        reply = select_reply(m.to_s, now)
         Service.primary.post(:message => "@#{m.user.idname} #{reply}", :replyto => m)
         m.favorite(true)
       end
