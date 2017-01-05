16ee21481402020478e081c47a6e38ed427b2b39
ahiru <wellingtonnmaeda0524@gmail.com>
2017-01-05 12:39:42 +0900

    リファクタ

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -2,51 +2,64 @@
 
 Plugin.create(:ahiru_yakuna) do
 
-  DEFINED_TIME = Time.new.freeze
   criminals = Set.new
   あひる焼き = %w(あひる焼き Ahiruyaki 扒家鸭)
 
-  # load reply dictionaries
-  begin
-    JAPANESE = YAML.load_file(File.join(__dir__, 'dictionary', 'japanese.yml'))
-    MESHITERO = YAML.load_file(File.join(__dir__, 'dictionary', 'meshitero.yml'))
-    ENGLISH = YAML.load_file(File.join(__dir__, 'dictionary', 'english.yml'))
-    CHINESE = YAML.load_file(File.join(__dir__, 'dictionary', 'chinese.yml'))
-  rescue LoadError
-    notice 'Could not load yml file'
+  def prepare
+    begin
+      @dictionary = Hash.new
+      dictionaries = Dir.glob("#{File.join(__dir__, 'dictionary')}/*")
+      dictionaries.each { |d| @dictionary[File.basename(d, '.*')] = YAML.load_file(d) }
+      @defined_time = Time.new.freeze
+    rescue LoadError => e
+      error "An Error Occurred: #{e}"
+    end
   end
+
+  def sample(key)
+    @dictionary.values_at(key)[0].sample
+  end
+
+
+  def select_reply(msg, time)
+    # お正月モード
+    return sample('shogatsu') if Time.now.yday <= 3
+    # 飯テロモード
+    return sample('meshitero') if (time >= 17 and time <= 19) or (time >= 0 and time <= 3)
+
+    # 言語ごとに使用辞書を変える
+    return sample('english') if msg =~ /Ahiruyaki/
+    return sample('chinese') if msg =~ /扒家鸭/
+    sample('japanese')
+  end
+
+
+  # load reply dictionaries
+  prepare
+
 
   filter_filter_stream_track do |watching|
     [(watching.split(','.freeze) + あひる焼き).join(',')]
   end
 
 
-  def select_reply(msg, time)
-    # 飯テロモード
-    if (time >= 17 && time <= 19) || (time >= 0 && time <= 3)
-      return MESHITERO.sample
-    end
-
-    # 言語ごとに使用辞書を変える
-    if msg =~ /Ahiruyaki/
-      return ENGLISH.sample
-    elsif msg =~ /扒家鸭/
-      return CHINESE.sample
-    else
-      return JAPANESE.sample
-    end
-  end
-
-
   on_appear do |ms|
     ms.each do |m|
-      if m.to_s =~ Regexp.union(あひる焼き) and m[:created] > DEFINED_TIME and !m.retweet? and !criminals.include?(m.id)
+      if m.to_s =~ Regexp.union(あひる焼き) and m[:created] > @defined_time and !m.retweet? and !criminals.include?(m.id)
         criminals << m.id
-        now = Time.now.hour
         # select reply dic & send reply & fav
-        reply = select_reply(m.to_s, now)
+        reply = select_reply(m.to_s, Time.now.hour)
         Service.primary.post(:message => "@#{m.user.idname} #{reply}", :replyto => m)
         m.favorite(true)
+      elsif m.to_s =~ /辞書更新/ and m[:created] > @defined_time and m.user.idname == 'ahiru3net'
+        Thread.new {
+          prepare
+        }.next { |_|
+          Service.primary.post(:message => "@ahiru3net 辞書の更新が完了しました: #{Time.now.to_s}")
+        }.trap { |err|
+          error err
+          Service.primary.post(:message => "@ahiru3net 辞書の更新時にエラーが発生しました: #{Time.now.to_s}")
+        }
       end
     end
   end
