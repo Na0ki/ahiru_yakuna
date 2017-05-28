c045e9653e5ed50efe5a3bf01af0cb087bb559f2
ahiru <wellingtonnmaeda0524@gmail.com>
2017-05-28 19:05:04 +0900

    アヒル焼き

--- ahiru_yakuna.rb
+++ ahiru_yakuna.rb
@@ -3,7 +3,7 @@
 
 Plugin.create(:ahiru_yakuna) do
   criminals = Set.new
-  あひる焼き = %w[あひる焼き アヒルヤキ Ahiruyaki 扒家鸭 3v.7g]
+  あひる焼き = %w[あひる焼き アヒルヤキ アヒル焼き Ahiruyaki 扒家鸭 3v.7g]
 
   # 辞書のロード
   def prepare
